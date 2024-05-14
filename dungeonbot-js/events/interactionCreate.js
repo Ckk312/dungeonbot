cb198aee11edd957cd02ce2dc3e15e12bf81cef5
Ckk312 <kdpena312@gmail.com>
2024-05-14 01:17:32 -0400

    updated interactionCreate.js
    checks if a message is deferred now as well as replied

--- dungeonbot-js/events/interactionCreate.js
+++ dungeonbot-js/events/interactionCreate.js
@@ -82,7 +82,7 @@ module.exports = {
             await command.execute(interaction);
         } catch (error) {
             console.error(error);
-            if (!interaction.replied) {
+            if (!interaction.replied && !interaction.deferred) {
                 await interaction.reply({ content: 'There was an error while executing this command!', ephemeral : true });
             } else {
                 await interaction.editReply('There was an error while executing this command!');
